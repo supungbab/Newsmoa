6e1d7fe8f9e2ad8955cb41cfb588f746dcf86f22
Kyeong Myeong Ho <77760217+supungbab@users.noreply.github.com>
2021-11-22 23:54:42 +0900

    Add files via upload

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -11,11 +11,14 @@ import io from 'socket.io-client';
 
 //https://blog.joonas.io/62 소켓 로그 해결
 //https://github.com/socketio/socket.io-client/issues/1097#issuecomment-301301030 참고 문헌
-const serverURL='http://localhost:8082'
+
+//https://warlord0blog.wordpress.com/2018/07/18/vue-cli-3-and-hmr/
+
+const serverURL='http://192.168.219.106:8082'
 const socket = io.connect(serverURL, { transports: ['websocket'] });
 Vue.prototype.$socket = socket;
 
-const serverURL2='http://localhost:8083'
+const serverURL2='http://192.168.219.106:8083'
 const socket2 = io.connect(serverURL2, { transports: ['websocket'] });
 Vue.prototype.$socket2 = socket2;
 //console.log(socket)
